424f831cbb44b388902f790db4701ae3d0a85cad
ydiller <ydiller100@gmail.com>
2020-09-15 11:45:31 +0300

    Added augmentation option

--- dataset.py
+++ dataset.py
@@ -5,15 +5,17 @@ import numpy as np
 
 
 class FliesDataset(object):
-    def __init__(self, root, boxes_path, transforms=None):
+    def __init__(self, root, boxes_path, transforms=None, augmentations=None):
         self.root = root
         self.boxes_file = pd.read_csv(boxes_path)
         self.image_paths = self.boxes_file['File path'].unique()
         self.transforms = transforms
+        self.augmentations = augmentations
 
     def __getitem__(self, idx):
         img_path = self.image_paths[idx]
         img = Image.open(img_path).convert("RGB")
+        img = np.asarray(img)
         boxes = []
         labels = []
         select_rows = self.boxes_file.loc[self.boxes_file['Index'] == idx]
@@ -26,8 +28,11 @@ class FliesDataset(object):
             label = row['Label']
             boxes.append([x, y, x + w, y + h])
             labels.append(label)
+        if self.augmentations is not None:
+            augmentations = self.augmentations(image=img, bboxes=boxes, category_ids=labels)
+            img, boxes, labels = augmentations['image'], augmentations['bboxes'], augmentations['category_ids']
         labels = np.asarray(labels)
-        labels = torch.from_numpy(labels.astype('long'))
+        labels = torch.from_numpy(labels.astype('int64')) #original: 'long'
         boxes = torch.as_tensor(boxes, dtype=torch.float32)
         image_id = torch.tensor([idx])
         area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
@@ -37,6 +42,8 @@ class FliesDataset(object):
 
         if self.transforms is not None:
             img, target = self.transforms(img, target)
+
+
 
         return img, target
 
