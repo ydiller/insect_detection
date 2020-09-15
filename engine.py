d91651df378785bc24be2480f8f492aab968f2e5
ydiller <ydiller100@gmail.com>
2020-09-15 11:44:51 +0300

    Adding loss values info

--- engine.py
+++ engine.py
@@ -26,7 +26,6 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
     for images, targets in metric_logger.log_every(data_loader, print_freq, header):
         images = list(image.to(device) for image in images)
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
-
         loss_dict = model(images, targets)
 
         losses = sum(loss for loss in loss_dict.values())
@@ -51,6 +50,8 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
 
         metric_logger.update(loss=losses_reduced, **loss_dict_reduced)
         metric_logger.update(lr=optimizer.param_groups[0]["lr"])
+
+    return loss_value
 
 
 def _get_iou_types(model):
